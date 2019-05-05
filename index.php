3bf75c84c77a000ad5b1b32581bafebecc19afdf
davidalejandrolerea <davidalejandrolerea@gmail.com>
2019-05-05 17:16:20 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+?>
+<html>
+	<head>
+		<title>.: CRUD :.</title>
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	</head>
+	<body>
+	<?php include "php/navbar.php"; ?>
+	<div class="container">
+	<div class="row">
+	<div class="col-md-12">
+		
+	</div>
+	</div>
+	</div>
+	</body>
+</html>
